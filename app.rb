994c355f5b47cdc10d9d28fd53b9d2631d3a06f5
Ryan Davey <ryancdavey@icloud.com>
2018-06-28 09:26:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,8 +4,10 @@ class App < Sinatra::Base
   
   get '/name' do
     "My name is  aaa"
+  end
   get '/hometown' do
     "My hometown is b"
+  end
   get '/favorite-song' do
     "My favorite song is c"
   end
